a8cceba18a29a6577658bfee4f7a1119762e8008
Wei <contact@wei-dev.com>
2026-08-18 17:42:07 +0200

    fix(core): rewrite all emitted ESM specifiers

--- packages/core/scripts/fix-esm-specifiers.mjs
+++ packages/core/scripts/fix-esm-specifiers.mjs
@@ -4,24 +4,24 @@ import { dirname, join } from "node:path";
 
 const here = dirname(fileURLToPath(import.meta.url));
 const dist = join(here, "..", "dist");
-const replacements = [
-  ["./anatomy\"", "./anatomy.js\""],
-  ["./collection\"", "./collection.js\""],
-  ["./controllers\"", "./controllers.js\""],
-  ["./dom\"", "./dom.js\""],
-  ["./ids\"", "./ids.js\""],
-  ["./registry\"", "./registry.js\""],
-  ["./store\"", "./store.js\""],
-  ["./anatomy'", "./anatomy.js'"],
-  ["./collection'", "./collection.js'"],
-  ["./controllers'", "./controllers.js'"],
-  ["./dom'", "./dom.js'"],
-  ["./ids'", "./ids.js'"],
-  ["./registry'", "./registry.js'"],
-  ["./store'", "./store.js'"],
+const modules = [
+  "anatomy",
+  "collection",
+  "controllers",
+  "dom",
+  "focus",
+  "form",
+  "ids",
+  "positioning",
+  "registry",
+  "store",
 ];
+const replacements = modules.flatMap((name) => [
+  [`./${name}\"`, `./${name}.js\"`],
+  [`./${name}'`, `./${name}.js'`],
+]);
 
-for (const file of ["index.js", "controllers.js", "index.d.ts", "controllers.d.ts"]) {
+for (const file of ["index.js", "controllers.js", "form.js", "index.d.ts", "controllers.d.ts", "form.d.ts"]) {
   const path = join(dist, file);
   let source = await readFile(path, "utf8");
   for (const [from, to] of replacements) source = source.split(from).join(to);
